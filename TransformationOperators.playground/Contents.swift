4a91ad38f3d26c3f9e9c4ee741720faa834027bb
Aaron Lee <aaronlab.net@gmail.com>
2020-10-17 16:51:03 +0900

    flatMap

--- TransformationOperators.playground/Contents.swift
+++ TransformationOperators.playground/Contents.swift
@@ -1,12 +1,17 @@
 import Foundation
 import Combine
 
-// Collect ---> Array
+/*
+ collect
+ */
 ["A", "B", "C", "D"].publisher.collect().sink { print($0) }
 ["A", "B", "C", "D", "E"].publisher.collect(3).sink { print($0) } // per 3
 
 print()
 
+/*
+ map
+ */
 let formatter = NumberFormatter()
 formatter.numberStyle = .spellOut
 
@@ -18,6 +23,9 @@ formatter.numberStyle = .spellOut
 
 print()
 
+/*
+ map KeyPath
+ */
 struct Point {
     let x: Int
     let y: Int
@@ -31,4 +39,32 @@ publisher.map(\.x, \.y).sink { x, y in
 
 publisher.send(Point(x: 10, y: 5))
 
+print()
 
+/*
+ flatmap
+ */
+struct School {
+    let name: String
+    let noOfStudents: CurrentValueSubject<Int, Never>
+    
+    init(name: String, noOfStudents: Int) {
+        self.name = name
+        self.noOfStudents = CurrentValueSubject(noOfStudents)
+    }
+}
+
+let citySchool = School(name: "Toronto Univ.", noOfStudents: 12345)
+
+let school = CurrentValueSubject<School, Never>(citySchool)
+
+school
+    .flatMap { $0.noOfStudents }
+    .sink { print($0) }
+
+let townSchool = School(name: "Woodbine School", noOfStudents: 80)
+
+school.value = townSchool
+
+citySchool.noOfStudents.value += 20000
+townSchool.noOfStudents.value += 10
