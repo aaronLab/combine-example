7d08955bdbaa9cf6c4ab47ecbdbd28e9c86dc31e
Aaron Lee <aaronlab.net@gmail.com>
2020-10-17 15:35:21 +0900

    Publisher

---
+++ HelloPublishers/HelloPublishers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloPublishers
+//
+//  Created by Aaron Lee on 2020-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let notification = Notification.Name("MyNotification")
+        
+        let publisher = NotificationCenter.default.publisher(for: notification, object: nil)
+        
+        let subscription = publisher.sink { _ in
+            print("Notification Received")
+        }
+        
+        NotificationCenter.default.post(name: notification, object: nil)
+        
+    }
+
+
+}
+
