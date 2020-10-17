ebc29fa79fa3af7bb2ffbce1fc50d7e5cfeea8da
Aaron Lee <aaronlab.net@gmail.com>
2020-10-17 17:09:42 +0900

    URLSession extensions

---
+++ Networking.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Combine
+
+func getPosts() -> AnyPublisher<Data, URLError> {
+
+    guard let url = URL(string: "http://jsonplaceholder.typicode.com/posts") else {
+        fatalError("Invalid URL")
+    }
+
+    return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }
+        .eraseToAnyPublisher()
+
+}
+
+let cancellable = getPosts().sink(receiveCompletion: {_ in }, receiveValue: { print($0) })
