c730e978d2e3a49bb8741bea84b5a3c0cec88521
Aaron Lee <aaronlab.net@gmail.com>
2020-10-17 16:42:01 +0900

    map KeyPath

--- TransformationOperators.playground/Contents.swift
+++ TransformationOperators.playground/Contents.swift
@@ -5,6 +5,7 @@ import Combine
 ["A", "B", "C", "D"].publisher.collect().sink { print($0) }
 ["A", "B", "C", "D", "E"].publisher.collect(3).sink { print($0) } // per 3
 
+print()
 
 let formatter = NumberFormatter()
 formatter.numberStyle = .spellOut
@@ -14,3 +15,20 @@ formatter.numberStyle = .spellOut
 }.sink {
     print($0)
 }
+
+print()
+
+struct Point {
+    let x: Int
+    let y: Int
+}
+
+let publisher = PassthroughSubject<Point, Never>()
+
+publisher.map(\.x, \.y).sink { x, y in
+    print("x is \(x), and y is \(y)")
+}
+
+publisher.send(Point(x: 10, y: 5))
+
+
