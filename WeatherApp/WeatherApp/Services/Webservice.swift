0904cb789ca00f20b5a1d619e392f7a705cc0e82
Aaron Lee <aaronlab.net@gmail.com>
2020-10-17 17:54:28 +0900

    Remove Cache

---
+++ WeatherApp/WeatherApp/Services/Webservice.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Aaron Lee on 2020-10-17.
+//
+
+import Foundation
+import Combine
+
+class Webservice {
+    
+}
---
+++ WeatherApp/WeatherApp/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Aaron Lee on 2020-10-17.
+//
+
+import Foundation
+
+struct Constants {
+    struct URLs {
+        static let weather = ""
+    }
+}
