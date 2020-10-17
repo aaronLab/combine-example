480d64afa0bbccd7ce1fdd21336329a06b3f15da
Aaron Lee <aaronlab.net@gmail.com>
2020-10-17 18:11:56 +0900

    Display Weather

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    private var webservice: Webservice = Webservice()
+    private var cancellable: AnyCancellable?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.temperatureLabel.text = "Loading..."
+
+        self.cancellable = self.webservice.fetchWeather(city: "London")
+            .catch { _ in Just(Weather.placeholder) }
+            .map { weather in
+                if let temp = weather.temp {
+                    return "\(temp) ℃"
+                }
+                return "Error getting weather!"
+            }.assign(to: \.text, on: temperatureLabel)
     }
 
 
