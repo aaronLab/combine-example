0bc412b08d909f24388cf5fe7261eebb69e58d3f
Aaron Lee <aaronlab.net@gmail.com>
2020-10-26 21:55:18 +0900

    Prepend

---
+++ Combining Operators/Prepend.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+//import Combine
+
+let numbers = (1...5).publisher
+let publisher2 = (500...510).publisher
+
+numbers.prepend(100, 101)
+    .prepend(-1, -2)
+    .prepend([45, 55])
+    .prepend(publisher2)
+    .sink {
+    print($0)
+}
