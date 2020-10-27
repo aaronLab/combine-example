101cab0504f08c9e3565c2c8969cc418a0de0053
Aaron Lee <aaronlab.net@gmail.com>
2020-10-27 23:09:02 +0900

    CombineLatest

---
+++ Combining Operators/MergeLatest.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Combine
+
+let publisher1 = PassthroughSubject<Int, Never>()
+let publisher2 = PassthroughSubject<String, Never>()
+
+publisher1.combineLatest(publisher2)
+    .sink {
+        print("P1: \($0), P2: \($1)")
+}
+
+publisher1.send(1)
+publisher2.send("A")
+publisher2.send("B")
+publisher1.send(2)
+publisher1.send(3)
+publisher1.send(4)
