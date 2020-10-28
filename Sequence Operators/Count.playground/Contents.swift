8cb3e368d9c28a4755100e0280e60dd012e379e7
Aaron Lee <aaronlab.net@gmail.com>
2020-10-28 20:45:35 +0900

    Count

--- Sequence Operators/Count.playground/Contents.swift
+++ Sequence Operators/Count.playground/Contents.swift
@@ -2,7 +2,20 @@ import Foundation
 import Combine
 
 let publisher = ["A", "B", "C", "D"].publisher
+let publisher2 = PassthroughSubject<Int, Never>()
 
 publisher.count().sink {
     print($0)
 }
+
+publisher2.count()
+    .sink {
+        print($0)
+    }
+
+publisher2.send(10)
+publisher2.send(10)
+publisher2.send(10)
+publisher2.send(10)
+publisher2.send(10)
+publisher2.send(completion: .finished)
